bdd840285adf0ed3a479563d999324bec46e1f3c
Martin Antunez <lmartin.antunez@gmail.com>
2021-04-09 10:58:33 -0400

    avance seed

--- src/infrastructure/server.ts
+++ src/infrastructure/server.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express';
+import express from 'express';
 import actuator from 'express-actuator';
 import path from 'path';
 import bodyParser from 'body-parser';
